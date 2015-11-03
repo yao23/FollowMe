94a9be24f2e4412c84cf04757b8acbd350d2d757
Yao Li <yli@sazze.com>
2015-11-02 20:38:52 -0800

    add four buttons

---
+++ FollowMe/FollowMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FollowMe
+//
+//  Created by Yao Li on 11/2/15.
+//  Copyright © 2015 MyCompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
