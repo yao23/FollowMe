d4829769b67fd8663d2cd2d4c41ef4902be3000b
Yao Li <yli@sazze.com>
2015-11-02 21:29:47 -0800

    define basic variables

--- FollowMe/FollowMe/ViewController.swift
+++ FollowMe/FollowMe/ViewController.swift
@@ -8,7 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIAlertViewDelegate {
+
+    enum ButtonColor : Int {
+        case Red = 1
+        case Green = 2
+        case Blue = 3
+        case Yellow = 4
+    }
+
+    enum WhoseTurn {
+        case Human
+        case Computer
+    }
+
+    // view related objects and variables
+    @IBOutlet weak var redButton : UIButton!
+    @IBOutlet weak var greenButton : UIButton!
+    @IBOutlet weak var blueButton : UIButton!
+    @IBOutlet weak var yellowButton : UIButton!
+
+    // model related objects and variables
+    let winningNumber : Int = 25
+    var currentPlayer : WhoseTurn = .Computer
+    var inputs = [ButtonColor]()
+    var indexOfNextButtonToTouch : Int = 0
+    var highlightSquareTime = 0.5
 
     override func viewDidLoad() {
         super.viewDidLoad()
